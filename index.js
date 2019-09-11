dd3c1ad908b846319762d5bf598da0c08f2c0a30
pdangelo4 <paulsdangelo@gmail.com>
2019-09-11 13:50:49 -0700

    mern first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Require needed packages
+let express = require('express')
+let cors = require('cors')
+// Instantiate app
+let app = express()
+
+// Set up middleware
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json({ limit: '30mb' }))
+
+// Routes
+app.get('*', (req, res) => {
+    res.status(404).send({ message: 'Not Found'})
+})
+
+app.listen(process.env.PORT || 3005)
